124c6f65446b231f2b9822265c9feeee5e7475e8
darrylamt <dtamoatey001@st.ug.edu.gh>
2023-03-09 05:56:15 -0800

    edit current working directory file to 2 lines

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# Get the absolute path name of the current working directory
-abs_path=$(readlink -f $(pwd))
-
-# Print the absolute path name
-echo "The absolute path name of the current working directory is: $abs_path"
-
+pwd
